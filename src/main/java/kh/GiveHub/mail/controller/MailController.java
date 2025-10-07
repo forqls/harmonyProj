7600fa01c0c0cba6a96b7ff31065821642759e7e
Hyobin An <dksggyqls@gmail.com>
2025-10-07 21:32:46 +0900

    mailcontroller 수정

--- src/main/java/kh/GiveHub/mail/controller/MailController.java
+++ src/main/java/kh/GiveHub/mail/controller/MailController.java
@@ -27,21 +27,46 @@ public class MailController {
     private final MailService mailService;
 
     @ResponseBody
-    @PostMapping("/emailCheck") // ajax로 보낸 email을 받아서 넘기기
-    public String emailCheck(@RequestParam("email") String mail) throws MessagingException, UnsupportedEncodingException {
-        return mailService.sendSimpleMessage(mail);
+    @PostMapping("/emailCheck")
+    public String emailCheck(@RequestParam("email") String mail) {
+        try {
+            return mailService.sendSimpleMessage(mail);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return "EMAIL_FAIL";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "UNKNOWN_FAIL";
+        }
     }
-    
+
     @ResponseBody
-    @PostMapping("/findIdemailCheck") // ajax로 보낸 email을 받아서 넘기기
-    public String emailCheck2(@RequestParam("email") String mail) throws MessagingException, UnsupportedEncodingException {
-        return mailService.sendSimpleMessage(mail);
+    @PostMapping("/findIdemailCheck")
+    public String emailCheck2(@RequestParam("email") String mail) {
+        try {
+            return mailService.sendSimpleMessage(mail);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return "EMAIL_FAIL";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "UNKNOWN_FAIL";
+        }
     }
     
     @ResponseBody
     @PostMapping("/findPwdemailCheck") // ajax로 보낸 email을 받아서 넘기기
-    public String emailCheck3(@RequestParam("email") String mail) throws MessagingException, UnsupportedEncodingException {
-        return mailService.sendSimpleMessage(mail);
+    public String emailCheck3(@RequestParam("email") String mail) {
+        try {
+            return mailService.sendSimpleMessage(mail);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return "EMAIL_FAIL";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "UNKNOWN_FAIL";
+        }
     }
     
 }
