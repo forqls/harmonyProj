75d07e519b21c2ae8aa721fe50cc3fd5408649d6
Hyobin An <dksggyqls@gmail.com>
2025-10-08 04:24:05 +0900

    시큐리티 - 조건문 바꿈

--- src/main/java/kh/GiveHub/common/config/SecurityConfig.java
+++ src/main/java/kh/GiveHub/common/config/SecurityConfig.java
@@ -77,14 +77,11 @@ public class SecurityConfig {
                 Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
                 // 2. 권한 목록에 'ADMIN' 문자열이 포함된 권한
-                boolean isAdmin = roles.stream()
-                        .anyMatch(role -> role.toUpperCase().contains("ADMIN"));
-
-                if (isAdmin) {
-                    // 관리자 권한이 확인되면 관리자 메인 페이지로 이동
+                if (roles.contains("ROLE_ADMIN")) {
+                    // 관리자 로그인 성공 -> 관리자 메인 페이지로 이동
                     response.sendRedirect("/admin/main");
                 } else {
-                    // 그 외 (일반 사용자)는 메인 페이지로 이동
+                    // 일반 사용자 로그인 성공 -> 메인 페이지로 이동
                     response.sendRedirect("/");
                 }
             }
