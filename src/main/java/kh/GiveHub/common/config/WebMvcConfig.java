7debbba7f693139fcd0f4fa33fc9e449a829f32a
Hyobin An <dksggyqls@gmail.com>
2025-10-17 19:19:44 +0900

    로컬파일 업로드 로직 주석처리

--- src/main/java/kh/GiveHub/common/config/WebMvcConfig.java
+++ src/main/java/kh/GiveHub/common/config/WebMvcConfig.java
@@ -4,32 +4,33 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer{
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer{
 
-	@Value("${upload.path}")
-	private String uploadPath;
-
-	public static String getBasePath() {
-		String os = System.getProperty("os.name").toLowerCase();
-		if (os.contains("win")) {
-			return "c:/GiveHub";
-		}else {
-			return "/Users/" + System.getProperty("user.name") + "/GiveHub";
-		}
-	}
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		String basePath = getBasePath();
-
-		registry.addResourceHandler("/temp/**")
-				.addResourceLocations("file:///"+basePath+"/temp/");
-		registry.addResourceHandler("/upload/**")
-				.addResourceLocations("file:///"+basePath+"/upload/");
-		registry.addResourceHandler("/**")
-				.addResourceLocations("classpath:/static/");
-		registry.addResourceHandler("/upload/**")
-				.addResourceLocations("file:///" + uploadPath);
-
-	}
-}
+	//로컬경로
+//	@Value("${upload.path}")
+//	private String uploadPath;
+//
+//	public static String getBasePath() {
+//		String os = System.getProperty("os.name").toLowerCase();
+//		if (os.contains("win")) {
+//			return "c:/GiveHub";
+//		}else {
+//			return "/Users/" + System.getProperty("user.name") + "/GiveHub";
+//		}
+//	}
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		String basePath = getBasePath();
+//
+//		registry.addResourceHandler("/temp/**")
+//				.addResourceLocations("file:///"+basePath+"/temp/");
+//		registry.addResourceHandler("/upload/**")
+//				.addResourceLocations("file:///"+basePath+"/upload/");
+//		registry.addResourceHandler("/**")
+//				.addResourceLocations("classpath:/static/");
+//		registry.addResourceHandler("/upload/**")
+//				.addResourceLocations("file:///" + uploadPath);
+//
+//	}
+//}
